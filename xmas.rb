01f44eb531ee764824f82f7c79d06e3c416e98f1
Yann Klein <yann.klein@me.com>
2023-03-30 17:59:09 +0900

    base code

---
+++ xmas.rb
@@ -0,0 +1,24 @@
+# Write a method which 
+# returns the number of days until next Xmas.
+# using TDD (Test driven development)
+
+# Opt1: return number of days for any date!
+# Opt2: make it work after Dec 25th
+
+
+#        ____________   
+#  [IN] | (params)   |  [OUT]
+# ----->|   method   |----------> 
+#  args |____________| returned
+#                       value 
+
+# Method name:
+# Method signature (params? returned value?):
+# params: 
+# returned: 
+
+# Code
+
+
+# Tests
+puts "-- Test 1 ⛄️ -- "
