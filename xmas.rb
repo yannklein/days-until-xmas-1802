f737acff8932469f1137c7e955202b8f444382ae
Yann Klein <yann.klein@me.com>
2023-09-28 22:24:57 +0900

    livecode done

--- xmas.rb
+++ xmas.rb
@@ -1,5 +1,10 @@
+# 🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️
+# ##########  DAYS BEFORE XMAS ###########
+# 🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️🎄⭐️
+
+
 # Write a method which 
-# returns the number of days until next Xmas.
+# returns the number of days until next Xmas
 # using TDD (Test driven development)
 
 # Opt1: return number of days for any date!
@@ -12,13 +17,45 @@
 #  args |____________| returned
 #                       value 
 
-# Method name:
+# Method name: days_until_xmas
 # Method signature (params? returned value?):
-# params: 
-# returned: 
+# params: no params (but day, month, year for opt1)
+# returned: integer
+
 
 # Code
+# First, we have to require date 
+# We need to make a method definition
+# no parameters
+# Define today
+# Define Xmas day
+# Return Xmas - Today
+# Change to integer 
+require "date"
 
+def days_until_xmas(someday = Date.today)
+  # today = Date.today
+  xmas = Date.new(someday.year,12,25)
+  return (xmas - someday).to_i 
+end
 
 # Tests
-puts "-- Test 1 ⛄️ -- "
+# consider that the method exists
+# puts "-- Test 1 ⛄️ -- "
+puts "should return an integer"
+puts "Result:"
+puts days_until_xmas().class == Integer
+
+# puts "-- Test 2 🎄 -- "
+puts "should return 88 today"
+puts "Result:"
+puts days_until_xmas == 88
+
+puts "-- Test 3 🎅 -- "
+puts "should return 0 on Xmas day"
+puts "Result:"
+puts days_until_xmas(Date.new(2021,12,25)) == 0
+
+# puts "-- Test 4 🎅 -- "
+
+# puts "-- Test 5 🎅 -- "
